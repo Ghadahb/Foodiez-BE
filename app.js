050e9ecf3396e87b5be9eaade9aa7c1ff31b2301
Ghadahb <ghada.dxb@gmail.com>
2021-11-08 16:47:26 +0300

    removed signout from backend

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const connectDB = require("./db/database");
+const morgan = require("morgan");
 const cors = require("cors");
 const logger = require("./middleware/logger");
+const errorHandler = require("./middleware/errorHandler");
 const userRoutes = require("./apis/user/users.routes");
 const app = express();
+const passport = require("passport");
+const { localStrategy } = require("./middleware/passport");
 
 app.use(cors());
 
@@ -11,14 +15,17 @@ connectDB();
 
 // Middleware
 app.use(express.json());
+app.use(morgan("dev"));
 app.use(logger);
 
-// Routes
+// Passport
+app.use(passport.initialize());
+passport.use(localStrategy);
 
+// Routes
 app.use("/api", userRoutes);
 
-
-
+app.use(errorHandler);
 
 const PORT = 8002;
 app.listen(PORT, () => console.log(`Application is running on localhost: ${PORT}`));
